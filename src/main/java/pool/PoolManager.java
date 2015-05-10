ac6c8b42d8ca915efe06c3f9fa3a7a5e272343e3
scamposb <scamposbuil@gmail.com>
2015-05-10 20:14:19 +0200

    Creado monitor para la gestión del pool

--- src/main/java/pool/PoolManager.java
+++ src/main/java/pool/PoolManager.java
@@ -1,10 +1,53 @@
 package pool;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Sistemas y Tecnologías Web. 2015.
- * Práctica 6.
+ * Pool de conexiones en MySQL
  *
- * @author Sandra Campos (629928)
+ * @author Sandra Campos
  */
 public class PoolManager {
+        /*
+         * Atributos de la clase
+         */
+        private List<Connection> conexiones;		//numero de conexiones disponibles
+
+
+        public PoolManager() throws SQLException{
+            conexiones = new ArrayList<Connection>();
+            for(int i=0; i<100;i++){
+                conexiones.add(ConnectionAdmin.getConnection());
+            }
+        }
+        /**
+         * Acción de coger una de las conexiones disponibles en el pool.
+         * @return instancia de conexion a la BD
+         */
+        public synchronized Connection getConnection(){
+            while(conexiones.isEmpty()){
+                try{
+                    wait();
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+            /* Hay al menos un recurso liberado, devolvemos instancia */
+            Connection conexion = conexiones.get(0);
+            conexiones.remove(0);
+            return conexion;
+        }
+        /**
+         * Devolvemos la conexion al array de recursos liberados
+         * @param conexion conexion a la BD que queremos liberar
+         */
+        public synchronized void returnConnection(Connection conexion) {
+            conexiones.add(conexion);
+            notifyAll();    //avisamos a todos los threads de la devolucion
+        }
+
 }
